a236390934067fdb53badc25e9cd3e9366c23868
Denis <denis-serov00@mail.ru>
2022-04-12 12:50:48 +0600

    add Function

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", '');
+let numberOfFilms;
+
+function start() {
+   numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
+
+  while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
+  }
+}
+start();
 
 let personalMovieDB = new Object({});
 personalMovieDB.count = numberOfFilms;
@@ -7,59 +16,89 @@ personalMovieDB.actors = {};
 personalMovieDB.genres = [];
 personalMovieDB.privat = false;
           
-          
-          
- for (let i = 0; i < 2; i++) {
-  let a = prompt("Один из полследних просмотренных фильмов?", ''),
-      b = prompt("На сколько оцените его?", ''); 
 
-      if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-      personalMovieDB.movies[a] = b;
-      console.log('done');
-      } else {
-       alert('Пожалуйста заполните все поля ввода!');
-       i--;
+          
+
+
+    function writeYourGenres() {
+      for (let i =1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
       }
     }
-       if (personalMovieDB.count < 10) {
-         alert('Просмотренно довольно мало фильмов');
-       } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        alert('Вы классический зритель'); 
-       } else if (personalMovieDB.count >= 30) {
-        alert('Вы киноман');
-
-        } else {
-        alert('Произошла ошибка!');
-       }
+    writeYourGenres();
 
 
-//    let num = 2;   
-//  do {
-//        let a = prompt("Один из полследних просмотренных фильмов?", '');
-//        let b = prompt("На сколько оцените его?", ''); 
+
+
+    function remembeMyFilms() {
+      for (let i = 0; i < 2; i++) {
+        let a = prompt("Один из полследних просмотренных фильмов?", ''),
+            b = prompt("На сколько оцените его?", ''); 
+      
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+            } else {
+             alert('Пожалуйста заполните все поля ввода!');
+             i--;
+            }
+          }
+        }
+        remembeMyFilms();
+
+
+
+    function detectPersinalLevel() {
+      if (personalMovieDB.count < 10) {
+        alert('Просмотренно довольно мало фильмов');
+      } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+      alert('Вы классический зритель'); 
+      } else if (personalMovieDB.count >= 30) {
+      alert('Вы киноман');
+
+      } else {
+      alert('Произошла ошибка!');
+      }
+    }
+    detectPersinalLevel();
+
+
+    function showMyDB(hidden) {
+      if (!hidden) {
+        console.log(personalMovieDB);
+      }
+    }
+    showMyDB(personalMovieDB.privat);
+
+/* Цикл с использованием do */ 
+
+
+// //    let num = 2;   
+// //  do {
+// //        let a = prompt("Один из полследних просмотренных фильмов?", '');
+// //        let b = prompt("На сколько оцените его?", ''); 
        
-//        num++;
-//        personalMovieDB.movies[a] = b;
-//       }
-//   while (num < 4);
+// //        num++;
+// //        personalMovieDB.movies[a] = b;
+// //       }
+// //   while (num < 4);
 
-//   switch (personalMovieDB.count) {
-//     case 10:
-//       alert('Просмотренно довольно мало фильмов');
-//   break;
-//     case    20:
-//       alert('Вы классический зритель');
-//   break;
-//     case  30:
-//       alert('Вы киноман');
-//   break;
-//   default:
-//     alert('Произошла ошибка!');
-//   break;
-//   }
+// //   switch (personalMovieDB.count) {
+// //     case 10:
+// //       alert('Просмотренно довольно мало фильмов');
+// //   break;
+// //     case    20:
+// //       alert('Вы классический зритель');
+// //   break;
+// //     case  30:
+// //       alert('Вы киноман');
+// //   break;
+// //   default:
+// //     alert('Произошла ошибка!');
+// //   break;
+// //   }
 
-         
-     
+      
       
     
 
